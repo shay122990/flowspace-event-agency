e2d486f23f27a9573aab09812c7fbe5c1a728c5a
shay asanova <moeshakasanova@gmail.com>
2025-09-29 08:17:19 +0400

    next config for future image imports

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+    ],
+  },
 };
-
-export default nextConfig;
+module.exports = nextConfig;
